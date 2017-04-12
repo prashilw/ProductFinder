9ad2423eab730d3cb97241dae7ae1d3419df7937
Prashil Wasnik <prashilw@gmail.com>
2017-04-12 14:48:47 +0530

    testing commit

---
+++ workspace/SpringM/src/com/prashil/controller/MainController.java
@@ -0,0 +1,87 @@
+package com.prashil.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.prashil.model.UserEntity;
+import com.prashil.repository.UserRepository;
+
+public class MainController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
+    public String getUsers(ModelMap modelMap) {
+        
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("userList", userList);
+
+        return "admin/users";
+    }
+
+    
+    @RequestMapping(value = "/admin/users/add", method = RequestMethod.GET)
+    public String addUser() {
+    	return "admin/addUser";
+    }
+
+    @RequestMapping(value = "/admin/users/addP", method = RequestMethod.POST)
+    public String addUserPost(@ModelAttribute("user") UserEntity userEntity) {
+
+    	//userRepository.save(userEntity);
+        System.out.println(userEntity.getFirstName());
+        System.out.println(userEntity.getLastName());
+
+        userRepository.saveAndFlush(userEntity);
+
+        return "redirect:/admin/users";
+    }
+
+    @RequestMapping(value = "/admin/users/show/{id}", method = RequestMethod.GET)
+    public String showUser(@PathVariable("id") Integer userId, ModelMap modelMap) {
+
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        modelMap.addAttribute("user", userEntity);
+        return "admin/userDetail";
+    }
+
+    @RequestMapping(value = "/admin/users/update/{id}", method = RequestMethod.GET)
+    public String updateUser(@PathVariable("id") Integer userId, ModelMap modelMap) {
+
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        modelMap.addAttribute("user", userEntity);
+        return "admin/updateUser";
+    }
+
+    @RequestMapping(value = "/admin/users/updateP", method = RequestMethod.POST)
+    public String updateUserPost(@ModelAttribute("userP") UserEntity user) {
+
+        userRepository.updateUser(user.getNickname(), user.getFirstName(),
+                user.getLastName(), user.getPassword(), user.getId());
+        userRepository.flush(); // åˆ·æ–°ç¼“å†²åŒº
+        return "redirect:/admin/users";
+    }
+
+    @RequestMapping(value = "/admin/users/delete/{id}", method = RequestMethod.GET)
+    public String deleteUser(@PathVariable("id") Integer userId) {
+
+        userRepository.delete(userId);
+        userRepository.flush();
+        return "redirect:/admin/users";
+    }
+
+}
